e7f10a9c785cceaa0c1b5d6934cc4ecd9c7c1c3d
Marcelo Soffredi <msoffredi@gmail.com>
2021-11-01 13:12:12 -0500

    Prevented operation delete if linked to any permission + tests

--- __tests__/handlers/operations.test.ts
+++ __tests__/handlers/operations.test.ts
@@ -4,6 +4,7 @@ import {
 } from '../utils/helpers';
 import { handler } from '../../src/handlers/auth';
 import { Operation } from '../../src/models/operation';
+import { Permission } from '../../src/models/permission';
 
 beforeEach(async () => {
     await addUserWithPermissions();
@@ -86,7 +87,7 @@ it('throws an error if calling POST without proper data', async () => {
     expect(postResult.statusCode).toEqual(400);
 });
 
-it('throws a 422 error if the id provided for a delete operation is not found', async () => {
+it('throws a 404 error if the id provided for a delete operation is not found', async () => {
     const deleteEvent = constructAuthenticatedAPIGwEvent(
         {},
         {
@@ -96,5 +97,29 @@ it('throws a 422 error if the id provided for a delete operation is not found', 
         },
     );
     const delResult = await handler(deleteEvent);
+    expect(delResult.statusCode).toEqual(404);
+});
+
+it('does not delete an operation if it is linked to an existing permission', async () => {
+    const permissions = await Permission.scan().exec();
+    expect(permissions).toBeDefined();
+    expect(permissions.length).toBeGreaterThan(0);
+
+    const operationId = permissions[0].operationId;
+    const operationBefore = await Operation.get(operationId);
+    expect(operationBefore).toBeDefined();
+
+    const deleteEvent = constructAuthenticatedAPIGwEvent(
+        {},
+        {
+            method: 'DELETE',
+            resource: '/v0/operations/{id}',
+            pathParameters: { id: operationId },
+        },
+    );
+    const delResult = await handler(deleteEvent);
     expect(delResult.statusCode).toEqual(422);
+
+    const operationAfter = await Operation.get(operationId);
+    expect(operationAfter).toBeDefined();
 });
